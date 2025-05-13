75f5666f56528f29ebab4de64fd8fa9507d866c3
Juan Esteban Meñaca <62906534+Dabcrash@users.noreply.github.com>
2025-05-12 23:24:48 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: 'class', // ← activamos el modo oscuro por clase
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
